77091672b8c9b1fa612fbd7e962c15ef091c4cc3
jeff27282 <44066472+jeff27282@users.noreply.github.com>
2022-12-22 04:23:59 +0800

    Update NTC_Temp-SerialOutput.h

--- NTC_Temp-SerialOutput/NTC_Temp-SerialOutput.h
+++ NTC_Temp-SerialOutput/NTC_Temp-SerialOutput.h
@@ -1,1 +1,16 @@
-examples
+#include <NTC_Temp.h>
+
+NTC_Temp temp1 = NTC_Temp(100000,4700,3950,A0,LOW);
+
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  float i = temp1.tempRead();
+  Serial.print(i);
+  Serial.println("°C");
+  delay(100);
+
+}
